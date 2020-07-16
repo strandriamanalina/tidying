c21f688dabbcf0f133261fee791863f2d825b962
strandriamanalina <strandriamanalina@gmail.com>
2020-07-16 09:33:07 +0300

    Downloading the dataset and loading data.

---
+++ run_analysis.R
@@ -0,0 +1,63 @@
+# Loading the packages
+library(tidyverse)
+
+# Downloading data
+# url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+# download.file(url, "Dataset.zip")
+
+
+# unzipping
+# unzip("Dataset.zip")
+# list.dirs("./UCI HAR Dataset")
+
+
+##--------------------------------------------------------------------
+##  Merging the training and the test sets to create one data set   -
+##--------------------------------------------------------------------
+# listing the files with their full paths
+directory <- "UCI HAR Dataset"
+list <- list.files(directory,pattern = ".txt" , recursive = TRUE, full.names = TRUE)
+# listing the filenames without the extension 
+filenames <- list %>%
+  basename() %>%
+  gsub(pattern =".txt",replacement = "") %>% 
+  tolower
+# putting all the data into a list and adding name
+data <- list %>% 
+  map(~read_table(., col_names =FALSE, progress = show_progress(), ))
+names(data) <- filenames
+
+# Merging test data
+test_data <-  data[grep("_test", names(data))]
+acceleration_test <- test_data[grep("body|total", names(test_data))]
+
+
+train_data <- data[grep("_train", names(data))]
+
+
+
+##-----------------------------------------------------------------------------------------
+##  Extracting the measurements on the mean and standard deviation for each measurement   -
+##-----------------------------------------------------------------------------------------
+
+
+
+##-----------------------------------------------------------------------------
+##  Using descriptive activity names to name the activities in the data set   -
+##-----------------------------------------------------------------------------
+
+
+
+##------------------------------------------------------------------------
+##  Appropriately labels the data set with descriptive variable names.   -
+##------------------------------------------------------------------------
+
+
+
+
+##----------------------------------------------------------------------------------------------------------------------------------------------------
+##  From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.   -
+##----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# 
